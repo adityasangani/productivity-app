b7e51bf1baa1abe522245b43263b9b756f874f89
unknown <ays12@iitbbs.ac.in>
2024-08-18 16:41:43 +0530

    changed layout structure

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import { LeftNav } from "../components/LeftNav";
 import { TopBar } from "../components/TopBar";
 import { RightBar } from "../components/RightBar";
 import { DashboardFrame } from "../components/DashboardFrame";
-// import { DFC1 } from "../components/DFC1";
+import { DFC1 } from "../components/DFC1";
 
 export const Dashboard = () => {
   return (
@@ -11,14 +11,9 @@ export const Dashboard = () => {
       <LeftNav />
       <TopBar />
       <RightBar />
-      {/* <div className="absolute mt-16 ml-60 p-5 w-8/12 min-h-screen overflow-hidden bg-yellow-500">
-        <div>
-          To adjust the border color to be lighter and add a nice shadow effect
-          to your dashboard components, you can modify the border and box-shadow
-          properties in your Tailwind CSS classes. Here's how you can do it:
-        </div>
-      </div> */}
-      <DashboardFrame />
+      <DashboardFrame>
+        <DFC1 />
+      </DashboardFrame>
     </>
   );
 };

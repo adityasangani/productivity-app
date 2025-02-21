0715110265492dd4a98b0307067a156f5f3b0627
unknown <ays12@iitbbs.ac.in>
2025-02-22 02:49:02 +0530

    added cors in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
+import cors from "cors";
 const express = require("express");
 const authRouter = require("./routes/auth/authRouter");
 const userRouter = require("./routes/user/userRouter");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+const allowedOrigins = [
+  "https://productivity-kcsjwzb0w-adityas-projects-57193f84.vercel.app/", //for vercel
+  "http://localhost:5173", //for local
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, //cookies or authentication
+    methods: ["GET", "POST", "PUT", "DELETE"], //allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], //allowed headers
+  })
+);
 
 app.use(express.json());
 

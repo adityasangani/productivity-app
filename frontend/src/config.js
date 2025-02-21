634035473c1a070afb5f22c3275e60aa7be2a935
unknown <ays12@iitbbs.ac.in>
2025-02-22 02:28:21 +0530

    changed config file

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,1 @@
-// export const BACKEND_URL = "http://localhost:8080";
-export const BACKEND_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8080"
-    : "https://your-backend-url.com"; // Replace with your deployed backend URL
-
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
